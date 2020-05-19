a46a3b0194be5b2ed1779a8464b7069f59a99a7c
sergey.konstantinovsky <yejonkov@gmail.com>
2020-05-19 16:03:48 +0300

    a

---
+++ ds/include/sorts.h
@@ -0,0 +1,8 @@
+
+#include <stddef.h>
+
+void BubbleSort(int *arr, size_t arr_length);
+
+void SelectionSort(int *arr, size_t arr_length);
+
+void InsertionSort(int *arr, size_t arr_length);
---
+++ ds/src/sorts/sorts.c
@@ -0,0 +1,10 @@
+
+/*****************************************
+**  Developer: Sergey Konstantinovsky   **
+**  Date:      19.05.2020               **
+**  Reviewer:  Eliran Abrevaya			**
+**  Status:    ???						**
+*****************************************/		
+
+#include "sorts.h"
+
